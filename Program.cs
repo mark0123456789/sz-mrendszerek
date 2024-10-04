cf075fd4db1386fc636d5e3abbc110f050d3f441
mark <piringerm@kkszki.hu>
2024-10-04 13:03:58 +0200

    begining

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamrendszerek
+{
+    internal class Program
+    {
+        public static void szamolas()
+        {
+            int szam = 0;
+            Console.Write("kérek a 10-es számrendszerben lévő számot: ");
+        }
+        static void Main(string[] args)
+        {
+           szamolas();
+        }
+    }
+}
+
