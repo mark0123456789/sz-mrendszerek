e5857a68f88ebec5afa9578114dda84c5ff05107
mark <piringerm@kkszki.hu>
2024-10-04 13:10:54 +0200

    bekeres competed

--- Program.cs
+++ Program.cs
@@ -8,14 +8,20 @@ namespace szamrendszerek
 {
     internal class Program
     {
-        public static void szamolas()
+        public static int bekeres()
         {
             int szam = 0;
             Console.Write("kérek a 10-es számrendszerben lévő számot: ");
+            szam = Convert.ToInt32(Console.ReadLine());
+            return szam;
+        }
+        public static void szamol()
+        {
+        
         }
         static void Main(string[] args)
         {
-           szamolas();
+
         }
     }
 }
