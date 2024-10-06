d761a84220584fd8fbb0c5ae4b83a809d4a2a688
PiringerM <piringerm@kkszki.hu>
2024-10-06 16:54:23 +0200

    működnie kéne

--- Program.cs
+++ Program.cs
@@ -1,43 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace szamrendszerek
 {
     internal class Program
     {
+
         public static int bekeres()
         {
             int szam = 0;
             Console.Write("kérek a 10-es számrendszerben lévő számot: ");
             szam = Convert.ToInt32(Console.ReadLine());
+
             return szam;
         }
         public static void szamol()
         {
-        List<int> list = new List<int>();
+            int Bekertszam = bekeres();
 
-            while (bekeres() != 0)
+            List<int> list = new List<int>();
+
+            while (Bekertszam != 0)
             {
-                if (bekeres() % 2 == 0)
+                if (Bekertszam % 2 == 0)
                 {
-                    list.Add(0);
+                    list.Add(1);
+                    Bekertszam %= 2;
                 }
                 else
                 {
-                    list.Add(1);
+                    list.Add(0);
+                    Bekertszam %= 2;
                 }
             }
+
             Console.WriteLine("a szám binárisban:");
-            for (int i = 0;)
-            Console.Write(
+
+            for (int i = list.Count - 1; i >= 0; i--)
+            {
+                Console.Write(list[i]);
+            }
+
+            Console.ReadKey();
 
         }
         static void Main(string[] args)
         {
-                szamol();
+            szamol();
         }
     }
 }
